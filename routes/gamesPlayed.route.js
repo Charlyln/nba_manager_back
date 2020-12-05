b08b5f7ef5c4da0147f3261f3183ae8edf0395f6
Charlyln <charleshenry.lenue>
2020-12-05 19:36:28 +0100

    add putProgressValuePerGame call for each game

--- routes/gamesPlayed.route.js
+++ routes/gamesPlayed.route.js
@@ -3,13 +3,15 @@ const gamesPlayed = express.Router()
 const gamePlayed = require('../middlewares/gamePlayed')
 const allGamePlayed = require('../middlewares/allGamePlayed')
 const twoGamePlayed = require('../middlewares/twoGamePlayed')
+const putProgressValuePerGame = require('../middlewares/putProgressValuePerGame')
 
-gamesPlayed.post('/:uuid/:SeasonUuid/:TeamUuid', async (req, res) => {
-  const { uuid, SeasonUuid, TeamUuid } = req.params
+gamesPlayed.post('/:uuid/:SeasonUuid/:TeamUuid/:UserUuid', async (req, res) => {
+  const { uuid, SeasonUuid, TeamUuid, UserUuid } = req.params
 
   try {
     const test = await gamePlayed(uuid)
     const TwoOtherGamePlayed = await twoGamePlayed(SeasonUuid, TeamUuid)
+    await putProgressValuePerGame(UserUuid)
     res.status(201).json(test)
   } catch (err) {
     res.status(422).json(err)
