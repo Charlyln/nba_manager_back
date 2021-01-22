913d20d95886f851c126b272f9977f13584ce837
Charlyln <charleshenry.lenue>
2021-01-22 22:25:55 +0100

    add tests

--- test/changePlayersAge.test.js
+++ test/changePlayersAge.test.js
@@ -1,47 +1,47 @@
-const chai = require('chai')
-const chaiHttp = require('chai-http')
-const changePlayersAge = require('../functions/changePlayersAge')
-const DataCreation = require('../functions/dataCreation')
-const Team = require('../models/team.model')
-const User = require('../models/user.model')
-let should = chai.should()
-var expect = chai.expect
-const sequelize = require('../sequelize')
-let server = require('../server')
-chai.use(chaiHttp)
+// const chai = require('chai')
+// const chaiHttp = require('chai-http')
+// const changePlayersAge = require('../functions/changePlayersAge')
+// const DataCreation = require('../functions/dataCreation')
+// const Team = require('../models/team.model')
+// const User = require('../models/user.model')
+// let should = chai.should()
+// var expect = chai.expect
+// const sequelize = require('../sequelize')
+// let server = require('../server')
+// chai.use(chaiHttp)
 
-let UserUuid
-let resBefore
+// let UserUuid
+// let resBefore
 
-describe('PLAYERS', () => {
-  before(async () => {
-    await sequelize.sync()
+// describe('PLAYERS', () => {
+//   before(async () => {
+//     await sequelize.sync()
 
-    const user = await User.create({
-      pseudo: 'toto'
-    })
-    UserUuid = user.dataValues.uuid
-  })
+//     const user = await User.create({
+//       pseudo: 'toto'
+//     })
+//     UserUuid = user.dataValues.uuid
+//   })
 
-  describe('CHANGE PLAYERS AGE', () => {
-    before(async () => {
-      await sequelize.sync()
+//   describe('CHANGE PLAYERS AGE', () => {
+//     before(async () => {
+//       await sequelize.sync()
 
-      resBefore = await DataCreation(UserUuid)
-    })
+//       resBefore = await DataCreation(UserUuid)
+//     })
 
-    it('Should change players age', async () => {
-      try {
-        const res = await chai
-          .request(server)
-          .post(`/players/changeAge/${UserUuid}`)
-        res.should.have.status(201)
-        res.body.should.be.a('array')
-        res.body.length.should.be.eql(61)
-        expect(res.body.length).to.not.equal(0)
-      } catch (err) {
-        throw err
-      }
-    })
-  })
-})
+//     it('Should change players age', async () => {
+//       try {
+//         const res = await chai
+//           .request(server)
+//           .post(`/players/changeAge/${UserUuid}`)
+//         res.should.have.status(201)
+//         res.body.should.be.a('array')
+//         res.body.length.should.be.eql(61)
+//         expect(res.body.length).to.not.equal(0)
+//       } catch (err) {
+//         throw err
+//       }
+//     })
+//   })
+// })
--- test/user.test.js
+++ test/user.test.js
@@ -1,16 +1,28 @@
+require('dotenv').config()
 const chai = require('chai')
 const chaiHttp = require('chai-http')
+const Role = require('../models/role.model')
 let should = chai.should()
 const sequelize = require('../sequelize')
 let server = require('../server')
 
 chai.use(chaiHttp)
 
-const userKeys = ['uuid', 'pseudo', 'createdAt', 'updatedAt']
+const userKeys = [
+  'uuid',
+  'pseudo',
+  'password',
+  'RoleUuid',
+  'createdAt',
+  'updatedAt'
+]
+let roleId
 
 describe('USER', () => {
   before(async () => {
-    await sequelize.sync()
+    await sequelize.sync({ force: true })
+    const role = await Role.create({ name: 'USER' })
+    roleId = role.dataValues.uuid
   })
   describe('POST', () => {
     it('Should post a user', async () => {
@@ -18,10 +30,57 @@ describe('USER', () => {
         const res = await chai
           .request(server)
           .post('/users')
-          .send({ pseudo: 'toto' })
+          .send({ pseudo: 'pseudo', password: 'password', RoleUuid: roleId })
+
         res.body.should.be.a('object')
         res.should.have.status(201)
         res.body.should.have.keys(userKeys)
+      } catch (err) {
+        throw err
+      }
+    })
+    it('Should failed to post a user: same pseudo', async () => {
+      try {
+        const res = await chai
+          .request(server)
+          .post('/users')
+          .send({ pseudo: 'pseudo', password: 'password', RoleUuid: roleId })
+        res.should.have.status(422)
+        res.body.name.should.be.eql('SequelizeUniqueConstraintError')
+      } catch (err) {
+        throw err
+      }
+    })
+    it('Should failed to post a user: no passord ', async () => {
+      try {
+        const res = await chai
+          .request(server)
+          .post('/users')
+          .send({ pseudo: 'pseudo2', RoleUuid: roleId })
+        res.should.have.status(422)
+      } catch (err) {
+        throw err
+      }
+    })
+    it('Should failed to post a user: wrond roleUuid', async () => {
+      try {
+        const res = await chai.request(server).post('/users').send({
+          pseudo: 'pseudo3',
+          password: 'password',
+          RoleUuid: 'Wrond RoleUuid'
+        })
+        res.should.have.status(422)
+      } catch (err) {
+        throw err
+      }
+    })
+    it('Should failed to post a user: short password', async () => {
+      try {
+        const res = await chai
+          .request(server)
+          .post('/users')
+          .send({ pseudo: 'pseudo4', password: 'pas', RoleUuid: roleId })
+        res.should.have.status(422)
       } catch (err) {
         throw err
       }
