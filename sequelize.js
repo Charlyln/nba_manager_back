7796d6b7f8af1977d8dbdf9c6a3ee192b0865924
Charlyln <charleshenry.lenue>
2021-01-22 19:10:26 +0100

    use node env to change database if test

--- sequelize.js
+++ sequelize.js
@@ -1,6 +1,14 @@
-require("dotenv").config();
-const Sequelize = require("sequelize");
-const { DBNAME, DBPASS, DBUSER, DBDIALECT, DBHOST } = process.env;
+require('dotenv').config()
+const Sequelize = require('sequelize')
+const {
+  DBNAME,
+  DBPASS,
+  DBUSER,
+  DBDIALECT,
+  DBHOST,
+  DBTEST,
+  NODE_ENV
+} = process.env
 
 if (process.env.DATABASE_URL) {
   module.exports = new Sequelize(process.env.DATABASE_URL, {
@@ -8,11 +16,11 @@ if (process.env.DATABASE_URL) {
   })
 } else {
   module.exports = new Sequelize({
-    host: DBHOST || "localhost",
+    host: DBHOST || 'localhost',
     username: DBUSER,
     password: DBPASS,
-    database: DBNAME,
-    dialect:  DBDIALECT,
-    logging: false,
-  });
+    database: NODE_ENV !== 'test' ? DBNAME : DBTEST,
+    dialect: DBDIALECT,
+    logging: false
+  })
 }
